8088d1361cec9eff65118a15334d781b33feb47b
RLT-G <inco.k.b.blizz@gmail.com>
2025-10-08 02:26:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+import express from "express";
+import { ExpressPeerServer } from "peer";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("✅ PeerJS server is running");
+});
+
+const server = http.createServer(app);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: "/",
+  allow_discovery: true,
+});
+
+app.use("/peerjs", peerServer);
+
+const PORT = 3003
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ PeerJS server running on port ${PORT}`);
+});
+
